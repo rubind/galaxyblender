87b66898d173249e22f3d6796a882b05808c486a
David Rubin <davidrubinlbl@gmail.com>
2018-06-09 14:05:59 -0400

    simple

--- simple_setup.py
+++ simple_setup.py
@@ -1,13 +1,11 @@
 import bpy
 
-#bpy.data.objects
-mesh = bpy.data.meshes.new(name="MyMesh")
+mesh = bpy.data.meshes.new(name="Plane")
 ob = bpy.data.objects.new("Test", mesh)
 scn = bpy.context.scene
 scn.objects.link(ob)
 scn.objects.active = ob
 
 
-print(mesh)
 
 #/Applications/Blender/blender.app/Contents/MacOS/blender --python simple_setup.py
